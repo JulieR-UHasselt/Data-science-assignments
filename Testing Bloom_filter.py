7dacb85db810ee4eed8e2ff1d76b8bef37fcd32a
JulieR-UHasselt <161957183+JulieR-UHasselt@users.noreply.github.com>
2024-06-16 18:47:40 +0100

    Renamed file - will be used for testing

---
+++ Testing Bloom_filter.py
@@ -0,0 +1,47 @@
+
+
+# Example datasets
+urls = [
+    "http://example.com",
+    "https://example.org",
+    "http://test.com",
+    "https://mywebsite.net",
+    "http://anotherexample.com",
+]
+
+emails = [
+    "example1@example.com",
+    "user2@test.org",
+    "contact@mydomain.net",
+    "admin@website.com",
+    "info@anotherexample.com",
+]
+
+ip_addresses = [
+    "192.168.0.1",
+    "10.0.0.2",
+    "172.16.0.3",
+    "8.8.8.8",
+    "127.0.0.1",
+]
+
+english_words = [
+    "apple",
+    "banana",
+    "cherry",
+    "date",
+    "elderberry",
+    "fig",
+    "grape",
+    "honeydew",
+    "kiwi",
+    "lemon",
+]
+
+dna_sequences = [
+    "AGCTTAGCTA",
+    "CGTAGCTAGC",
+    "TGCATGCACT",
+    "GCTAGCTAGC",
+    "TAGCTAGCTA",
+]
